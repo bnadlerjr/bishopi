a8b607bac048b448b1f398eab171f9abb356e48b
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2011-02-03 13:13:38 -0500

    Seed file for crawler.

--- lib/bishopi.rb
+++ lib/bishopi.rb
@@ -1,11 +1,17 @@
 module Bishopi
   class Crawler
-    def initialize(output=STDOUT)
+    attr_reader :worklist
+
+    def initialize(seed_filename, output=STDOUT)
       @output = output
+      @worklist = File.readlines(seed_filename).each do |line|
+        line.chomp!
+      end
     end
 
     def start
       @output.puts "Bishopi starting to crawl..."
+      @output.puts "Processing '#{@worklist[0]}'"
     end
   end
 end
--- spec/bishopi_spec.rb
+++ spec/bishopi_spec.rb
@@ -1,14 +1,44 @@
 require "#{File.dirname(__FILE__)}/../lib/bishopi"
 
 module Bishopi
-  describe "#start" do
-    it "shows a welcome message" do
-      output = double('output')
-      output.should_receive(:puts).
-        with('Bishopi starting to crawl...')
+  SAMPLE_URL_FILE = "#{File.dirname(__FILE__)}/sample-url-file.txt"
 
-      spider = Crawler.new(output)
-      spider.start
+  describe Crawler do
+    before do
+      @output = double('output').as_null_object
+    end
+
+    describe "#initialize" do
+      it "creates a worklist from a seed file" do
+        spider = Crawler.new(SAMPLE_URL_FILE, @output)
+        spider.worklist.should_not be_nil
+      end
+    end
+
+    describe "#start" do
+      it "shows a welcome message" do
+        @output.should_receive(:puts).
+          with('Bishopi starting to crawl...')
+
+        spider = Crawler.new(SAMPLE_URL_FILE, @output)
+        spider.start
+      end
+
+      it "stops when all URLs in worklist have been crawled"
+    end
+
+    describe "#process" do
+      it "shows the URL being processed" do
+        @output.should_receive(:puts).
+          with("Processing 'http://bobnadler.com'")
+
+        spider = Crawler.new(SAMPLE_URL_FILE, @output)
+        spider.start
+      end
+
+      it "retrieves a list of links on the page"
+      it "indexes the page if it doesn't exist"
+      it "does not index the page if robots are not allowed"
     end
   end
 end
